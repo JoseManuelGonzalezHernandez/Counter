e79e7a0d29f06abc1d95ff62e7c5682fe021b73a
José Manuel González Hernández <usr2275@salesianos-lacuesta.net>
2021-05-20 12:33:55 +0100

    Se ha realizado la prueba testValueCounter fallida

--- src/Counter.java
+++ src/Counter.java
@@ -1,49 +1,51 @@
+import java.util.Scanner;
+
 public class Counter {
-    int valInicial;
-    int valIncrement;
-    int valLimit;
+    final int valInicial;
+    final int valIncrement;
+    final int valLimit;
+    int counter;
+
+    Scanner entrada = new Scanner(System.in);
 
     public int getValInicial() {
         return this.valInicial;
-    }
-
-    public void setValInicial(int valInicial) {
-        this.valInicial = valInicial;
     }
 
     public int getValIncrement() {
         return this.valIncrement;
     }
 
-    public void setValIncrement(int valIncrement) {
-        this.valIncrement = valIncrement;
-    }
-
     public int getValLimit() {
         return this.valLimit;
-    }
-
-    public void setValLimit(int valLimit) {
-        this.valLimit = valLimit;
     }
 
     public Counter(int valInicial, int valIncrement, int valLimit) {
         this.valInicial = valInicial;
         this.valIncrement = valIncrement;
         this.valLimit = valLimit;
+        this.counter = valInicial;
     }
 
     public Counter(int valLimit) {
         this.valInicial = 0;
         this.valIncrement = 1;
         this.valLimit = valLimit;
+        this.counter = valInicial;
     }
 
     public int [] getVals() {
         int [] vals = {getValInicial(), getValIncrement(), getValLimit()};
         return vals;
     }
-    public static void main(String[] args) {
-        
+
+    public void doCounter() {
+        this.counter += this.valIncrement;
+    }
+
+    public void resetCounter() {
+        this.counter += valInicial;
     }
 }
+
+
--- src/counterTest.java
+++ src/counterTest.java
@@ -20,4 +20,32 @@ public class counterTest {
 
         assertEquals(valExpected, counter.getVals());
     }
+
+    public void testValueCounter() {
+        Counter counter = new Counter(10);
+
+        assertEquals(0,counter.counter);
+
+        System.out.println(counter.counter);
+        counter.doCounter();
+        System.out.println(counter.counter);
+
+        assertEquals(1,counter.counter);
+
+        counter.resetCounter();
+        
+
+        Counter counter2 = new Counter(1, 2, 20);
+
+        assertEquals(1,counter2.counter);
+
+        System.out.println(counter2.counter);
+        counter.doCounter();
+        System.out.println(counter2.counter);
+
+        assertEquals(3,counter2.counter);
+
+        counter2.resetCounter();
+
+    }
 }
